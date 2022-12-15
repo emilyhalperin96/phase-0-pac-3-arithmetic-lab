a3e62310f165e03766cf99b7de8d44936ff30f0d
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP.lan>
2022-12-15 14:26:12 -0500

    commit tree e9989135aa797a5c9038b1ceb789c5b012a5b831

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b
+  }
+  function multiply(a, b) {
+    return a * b
+  }
+  function divide(a, b) {
+    return a / b
+  }
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
+
+var n = 0;
+function increment(n){
+  n++;
+  return n;
+}
+var n = 0;
+function decrement(n){
+  n--;
+  return n;
+}Z$
